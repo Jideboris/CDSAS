b27c8ee33f0bafd4e16986586a5cae5daa6e2b92
Anthony Borisade <anthonyborisade@anthonys-mbp.home>
2018-07-09 12:59:22 +0100

    add employee

---
+++ src/app/admin.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core'
+import { of } from 'rxjs'
+import { Employee } from '../model/Employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor() { }
+  public getemployees() {
+
+    let fakeUsers = [{ empid: 1, firstname: 'Dhiraj', lastname: 'Ray', email: 'dhiraj@gmail.com' },
+    { empid: 1, firstname: 'Tom', lastname: 'Jac', email: 'Tom@gmail.com' },
+    { empid: 1, firstname: 'Hary', lastname: 'Pan', email: 'hary@gmail.com' },
+    { empid: 1, firstname: 'praks', lastname: 'pb', email: 'praks@gmail.com' },
+    ];
+
+    return of<Employee[]>(fakeUsers)
+
+
+  }
+}
