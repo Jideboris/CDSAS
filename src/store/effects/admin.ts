79a80286c987a96b0f5f5807e43e83bb2f392326
Anthony Borisade <anthonyborisade@anthonys-mbp.home>
2018-07-10 07:32:35 +0100

    effect

--- src/store/effects/admin.ts
+++ src/store/effects/admin.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core'
 import { Effect, Actions } from '@ngrx/effects'
 import { Action } from '@ngrx/store'
-import { Observable } from 'rxjs'
-
+import { Observable, of } from 'rxjs'
 import * as fromActions from '../actions/admin'
 import { AdminService } from '../../app/admin.service'
+import 'rxjs/add/observable/empty'
+import { map, switchMap, catchError } from "rxjs/operators"
 
-import { map, switchMap } from "rxjs/operators"
 
 @Injectable()
 export class EmployeeEffects {
@@ -18,13 +18,18 @@ export class EmployeeEffects {
     retrieveemployees$: Observable<Action> = this.actions$
         .ofType<fromActions.GetEmployeesAction>(fromActions.GET_EMPLOYEES)
         .pipe(switchMap(() => this.adminService.getemployees()))
-        .pipe(map(res => new fromActions.GetEmployeesDoneAction(res)))
-      //  .pipe(catch((new fromActions.GetEmployeeFailedAction(error)))
-      //  .map(action => action.payload)
-       // .switchMap(() => this.adminService.getemployees()
-            // .map(res => new fromActions.GetEmployeesDoneAction(res))
-            //  .catch(error => of(new fromActions.GetEmployeeFailedAction(error)))
-      //  );
+        .pipe(
+            map(result => {
+                if (!result) {
+                    return new fromActions.GetEmployeeFailedAction('Could not fetch data!')
+                } else {
+                    return new fromActions.GetEmployeesDoneAction(result)
+                }
+            }),
+            catchError((err, caught) => {
+                return of(err)
+            })
+        )
 
 
 }
