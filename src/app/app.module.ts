8e8ae60e697bc73dc709c7200e84f48f9ca39935
Anthony Borisade <anthonyborisade@anthonys-mbp.home>
2018-07-11 08:01:30 +0100

    styling and formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,17 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http'
 import { AppComponent } from './app.component'
 import { AdminModule } from './admin/admin.module'
-import { AuthGuard } from './authguard.service';
-import { BoardComponent } from './board/board.component';
-import { AnswerComponent } from './answer/answer.component';
-import { LoginComponent } from './login/login.component';
-import { AnswerSingleComponent } from './answer-single/answer-single.component';
-import { AnswerMultipleComponent } from './answer-multiple/answer-multiple.component';
+import { AuthGuard } from './authguard.service'
+import { BoardComponent } from './board/board.component'
+import { LoginComponent } from './login/login.component'
+import { AnswerSingleComponent } from './answer-single/answer-single.component'
+import { AnswerMultipleComponent } from './answer-multiple/answer-multiple.component'
 import { AnswerWriteComponent } from './answer-write/answer-write.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
-    AnswerComponent,
     LoginComponent,
     AnswerSingleComponent,
     AnswerMultipleComponent,
