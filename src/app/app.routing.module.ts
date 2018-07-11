346daded403cd07c4a8f64f9abc66410a2dcbcc9
Anthony Borisade <anthonyborisade@anthonys-mbp.home>
2018-07-11 08:26:57 +0100

    styling

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,14 +2,20 @@ import { AuthGuard } from './authguard.service'
 import { Routes } from '@angular/router'
 import { CreateadminComponent } from './admin/createadmin/createadmin.component'
 import { AdminemployeesComponent } from './admin/adminemployees/adminemployees.component'
+import {LoginComponent} from './login/login.component'
 export const routes: Routes = [
     {
         path: '',
+        //canActivate: [AuthGuard],
         component: CreateadminComponent
     },
     {
         path: 'addemployees',
        // canActivate: [AuthGuard],
         component: AdminemployeesComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
     }
 ];
