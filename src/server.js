3e84075e0bd686b3c068f6ef930fc5c184f950f0
Anthony Borisade <anthonyborisade@anthonys-mbp.home>
2018-08-04 09:28:20 +0100

    deplo issues

--- src/server.js
+++ src/server.js
@@ -13,4 +13,7 @@ res.sendFile(path.join(__dirname+'/dist/cdsas-project/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
+var server = app.listen(process.env.PORT || 8080, ()=> {
+    console.log('Express server listening on port ' + server.address().port)
+})
