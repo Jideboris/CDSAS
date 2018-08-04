9adb3e021a0fef20b79f655264db7878ec989b6e
Anthony Borisade <anthonyborisade@anthonys-mbp.home>
2018-08-04 10:30:58 +0100

    still issues

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/'));
+//app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join('/dist/index.html'));
 
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+   // res.sendFile(path.join(__dirname + '/dist/cdsas-project/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 //app.listen(process.env.PORT || 8080);
-var server = app.listen(process.env.PORT || 8080, ()=> {
+var server = app.listen(process.env.PORT || 8080, () => {
     console.log('Express server listening on port ' + server.address().port)
 })
