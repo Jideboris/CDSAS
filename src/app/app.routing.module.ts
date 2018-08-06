0c8335b4330c9c737db39440e81f76d7aa1be0e7
Anthony Borisade <anthonyborisade@anthonys-mbp.home>
2018-08-06 20:01:14 +0100

    applying guard

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,13 +3,13 @@ import { Routes } from '@angular/router'
 import { CreateadminComponent } from './admin/createadmin/createadmin.component'
 import { AdminemployeesComponent } from './admin/adminemployees/adminemployees.component'
 
-import {LoginComponent} from './login/log-in/login.component'
-import {AnswerMultipleComponent} from './answer-multiple/answer-multiple.component'
-import {AnswerSingleComponent} from './answer-single/answer-single.component'
-import {AnswerWriteComponent} from './answer-write/answer-write.component'
-import {CyberIssuesComponent} from './cyber-issues/cyber-issues.component'
-import {BoardComponent} from './board/board.component'
-import {ClientsComponent} from './clients/clients.component'
+import { LoginComponent } from './login/log-in/login.component'
+import { AnswerMultipleComponent } from './answer-multiple/answer-multiple.component'
+import { AnswerSingleComponent } from './answer-single/answer-single.component'
+import { AnswerWriteComponent } from './answer-write/answer-write.component'
+import { CyberIssuesComponent } from './cyber-issues/cyber-issues.component'
+import { BoardComponent } from './board/board.component'
+import { ClientsComponent } from './clients/clients.component'
 
 
 
@@ -19,12 +19,12 @@ import { ClientRegComponent } from './client-reg/client-reg.component';
 export const routes: Routes = [
     {
         path: 'client/:client-code',
-        //canActivate: [AuthGuard],
+        canActivate: [AuthGuard],
         component: CreateadminComponent
     },
     {
         path: 'addemployees',
-        // canActivate: [AuthGuard],
+        canActivate: [AuthGuard],
         component: AdminemployeesComponent
     },
     {
@@ -34,30 +34,36 @@ export const routes: Routes = [
 
     {
         path: 'answersingle',
+        canActivate: [AuthGuard],
         component: AnswerSingleComponent
     },
     {
         path: 'answermultiple',
+        canActivate: [AuthGuard],
         component: AnswerMultipleComponent
     },
     {
         path: 'answerwrite',
+        canActivate: [AuthGuard],
         component: AnswerWriteComponent
     },
     {
-        path: 'clientreg',
+        path: '',
         component: ClientRegComponent
     },
     {
         path: 'cyberissues',
+        canActivate: [AuthGuard],
         component: CyberIssuesComponent
     },
     {
         path: 'board',
+        canActivate: [AuthGuard],
         component: BoardComponent
     },
     {
         path: 'clients',
+        canActivate: [AuthGuard],
         component: ClientsComponent
     },
     { path: '**', component: PagenotfoundComponent }
