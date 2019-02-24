4099722684b21e80ae012960873fb359488b7fe3
Anthony <jideboris@yahoo.com>
2019-02-24 16:02:15 +0000

    change file from local to live

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,30 +1,30 @@
-export const SAVE_ADMIN_URL = "http://localhost:3000/api/v1/saveadmin"
-export const SAVE_CLIENT_REGISTRATION_URL = "http://localhost:3000/api/v1/saveclientreg"
-export const MESSAGE_EMPLOYEES_CLIENTS_EMAIL_URL = 'http://localhost:3000/api/v1/messageclientemployees'
-export const SAVE_CLIENT_REGISTRATION_FORM_URL = "http://localhost:3000/api/v1/saveclientregistration"
-export const SAVE_CLIENT_EMPLOYEES_URL = "http://localhost:3000/api/v1/saveclientemployees"
-export const SEND_CLIENT_EMAIL_URL = "http://localhost:3000/api/v1/sendemail"
+// export const SAVE_ADMIN_URL = "http://localhost:3000/api/v1/saveadmin"
+// export const SAVE_CLIENT_REGISTRATION_URL = "http://localhost:3000/api/v1/saveclientreg"
+// export const MESSAGE_EMPLOYEES_CLIENTS_EMAIL_URL = 'http://localhost:3000/api/v1/messageclientemployees'
+// export const SAVE_CLIENT_REGISTRATION_FORM_URL = "http://localhost:3000/api/v1/saveclientregistration"
+// export const SAVE_CLIENT_EMPLOYEES_URL = "http://localhost:3000/api/v1/saveclientemployees"
+// export const SEND_CLIENT_EMAIL_URL = "http://localhost:3000/api/v1/sendemail"
 
-export const GET_SUBSCRIPTION_URL = "https://cdsas-api.herokuapp.com/api/v1/subscription"//"http://localhost:3000/api/v1/subscription"
-export const GET_CLIENTREG_URL = "http://localhost:3000/api/v1/clientreg/"
-export const GET_LOGININFO_URL = "http://localhost:3000/api/v1/logininfo/"
-export const GET_CLIENT_EMPLOYEES_URL = "http://localhost:3000/api/v1/clientemployees/"
+// export const GET_SUBSCRIPTION_URL = "https://cdsas-api.herokuapp.com/api/v1/subscription"//"http://localhost:3000/api/v1/subscription"
+// export const GET_CLIENTREG_URL = "http://localhost:3000/api/v1/clientreg/"
+// export const GET_LOGININFO_URL = "http://localhost:3000/api/v1/logininfo/"
+// export const GET_CLIENT_EMPLOYEES_URL = "http://localhost:3000/api/v1/clientemployees/"
 
-export const DELETE_CLIENT_EMPLOYEES_URL = "http://localhost:3000/api/v1/clientemployees/"
+// export const DELETE_CLIENT_EMPLOYEES_URL = "http://localhost:3000/api/v1/clientemployees/"
 
 
-// export const SAVE_ADMIN_URL = "https://cdsas-api.herokuapp.com/api/v1/saveadmin"
-// export const SAVE_CLIENT_REGISTRATION_URL = "https://cdsas-api.herokuapp.com/api/v1/saveclientreg"
-// export const SAVE_CLIENT_REGISTRATION_FORM_URL="https://cdsas-api.herokuapp.com/api/v1/saveclientregistration"
-// export const SAVE_CLIENT_EMPLOYEES_URL = "https://cdsas-api.herokuapp.com/api/v1/saveclientemployees"
-// export const SEND_CLIENT_EMAIL_URL = "https://cdsas-api.herokuapp.com/api/v1/sendemail"
+export const SAVE_ADMIN_URL = "https://cdsas-api.herokuapp.com/api/v1/saveadmin"
+export const SAVE_CLIENT_REGISTRATION_URL = "https://cdsas-api.herokuapp.com/api/v1/saveclientreg"
+export const SAVE_CLIENT_REGISTRATION_FORM_URL="https://cdsas-api.herokuapp.com/api/v1/saveclientregistration"
+export const SAVE_CLIENT_EMPLOYEES_URL = "https://cdsas-api.herokuapp.com/api/v1/saveclientemployees"
+export const SEND_CLIENT_EMAIL_URL = "https://cdsas-api.herokuapp.com/api/v1/sendemail"
 
-// export const GET_SUBSCRIPTION_URL = "https://cdsas-api.herokuapp.com/api/v1/subscription"//"https://cdsas-api.herokuapp.com/api/v1/subscription"
-// export const GET_CLIENTREG_URL = "https://cdsas-api.herokuapp.com/api/v1/clientreg/"
-// export const GET_LOGININFO_URL = "https://cdsas-api.herokuapp.com/api/v1/logininfo/"
-// export const GET_CLIENT_EMPLOYEES_URL = "https://cdsas-api.herokuapp.com/api/v1/clientemployees/"
+export const GET_SUBSCRIPTION_URL = "https://cdsas-api.herokuapp.com/api/v1/subscription"//"https://cdsas-api.herokuapp.com/api/v1/subscription"
+export const GET_CLIENTREG_URL = "https://cdsas-api.herokuapp.com/api/v1/clientreg/"
+export const GET_LOGININFO_URL = "https://cdsas-api.herokuapp.com/api/v1/logininfo/"
+export const GET_CLIENT_EMPLOYEES_URL = "https://cdsas-api.herokuapp.com/api/v1/clientemployees/"
 
-// export const DELETE_CLIENT_EMPLOYEES_URL = "https://cdsas-api.herokuapp.com/api/v1/clientemployees/"
+export const DELETE_CLIENT_EMPLOYEES_URL = "https://cdsas-api.herokuapp.com/api/v1/clientemployees/"
 
 
 
