7d6cd97b45bc20a862e1245d036897b7519be241
Anthony <jideboris@yahoo.com>
2019-02-24 15:23:00 +0000

    about us update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 import { Store } from '@ngrx/store'
 import { AdminState } from './store/reducers/admin'
 import * as fromActions from './store/actions/admin'
@@ -8,8 +8,7 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  ngOnInit() { }
+export class AppComponent {
   title = 'app'
   logstatus = 'Log out'
   constructor(private store: Store<AdminState>, private router: Router, ) {
